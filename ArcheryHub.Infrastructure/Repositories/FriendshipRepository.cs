61004d997852cbf707b18443efbd000e69ae74aa
MykhailoLucenko <lutsenko.mykhailo.nikolayevich@gmail.com>
2026-05-03 23:45:09 +0300

    Створив необхідну реалізацію репозиторіїв а також допрацював їхні інтерфейси.

---
+++ ArcheryHub.Infrastructure/Repositories/FriendshipRepository.cs
@@ -0,0 +1,42 @@
+﻿using ArcheryHub.Core.Entities.Users;
+using ArcheryHub.Core.Interfaces.Users;
+using ArcheryHub.Infrastructure.Database;
+using Dapper;
+
+namespace ArcheryHub.Infrastructure.Repositories;
+
+public class FriendshipRepository : IFriendshipRepository
+{
+    private readonly DapperContext _context;
+
+    public FriendshipRepository(DapperContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddFriendAsync(int userId, int friendId)
+    {
+        var query = "CALL sp_AddFriend(@UserId, @FriendId);";
+
+        using var connection = _context.CreateConnection();
+
+        await connection.ExecuteAsync(query, new { UserId = userId, FriendId = friendId });
+        
+    }
+
+    public async Task<IEnumerable<FriendActivity>> GetFriendActivity(int userId)
+    {
+        var query =
+            @"SELECT nickname AS Nickname, 
+            user_id AS UserId, 
+            `MAX(round_participants.joined_at)` AS LastActive 
+            FROM vw_FriendActivity WHERE user_id = @UserId;";
+
+        using var connection = _context.CreateConnection();
+
+        var response = await connection.QueryAsync<FriendActivity>(query, new { UserId = userId });
+
+        return response;
+
+    }
+}
---
+++ ArcheryHub.Infrastructure/Repositories/RoundRepository.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+using ArcheryHub.Core.Entities.Rounds;
+using ArcheryHub.Core.Interfaces.Rounds;
+using ArcheryHub.Infrastructure.Database;
+using Dapper;
+
+namespace ArcheryHub.Infrastructure.Repositories;
+
+public class RoundRepository : IRoundRepository
+{
+    private readonly DapperContext _context;
+
+    public RoundRepository(DapperContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> CreateRoundAsync(Round request)
+    {
+        var query = "CALL sp_CreateRound(@CreatedByUserId, @Title, @Distance, @TotalRoundEnds);";
+
+        using var connection = _context.CreateConnection();
+
+        var response = await connection.QuerySingleOrDefaultAsync<int>(query, param: request);
+
+        return response;
+
+    }
+
+    public async Task<EndResponse?> SubmitEndAsync(EndRequest request)
+    {
+        var query = "CALL sp_SubmitEnd(@UserId, @RoundId, @ArrowsJson);";
+        
+        string jsonArrows = JsonSerializer.Serialize(request.Arrows);
+        
+        using var connection = _context.CreateConnection();
+
+        var response = await connection.QueryFirstOrDefaultAsync<EndResponse>(query, new { UserId = request.UserId, RoundId = request.RoundId, ArrowsJson = jsonArrows });
+
+        return response;
+
+    }
+
+    public async Task<IEnumerable<RoundLeaderBoard>> GetRoundLeaderBoardAsync(int roundId)
+    {
+        var query = "CALL sp_GetRoundLeaderBoard(@RoundId)";
+
+        using var connection = _context.CreateConnection();
+
+        var response = await connection.QueryAsync<RoundLeaderBoard>(query, new { RoundId = roundId });
+
+        return response;
+
+    }
+
+    public async Task<IEnumerable<GlobalRecords>> GetGlobalRecordsAsync()
+    {
+        var query = "SELECT * FROM vw_GlobalRecords;";
+
+        using var connection = _context.CreateConnection();
+
+        var response = await connection.QueryAsync<GlobalRecords>(query);
+
+        return response;
+
+    }
+}
